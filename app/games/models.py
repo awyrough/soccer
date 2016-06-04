030fbf165c369876ff9f4c3133282cd5d27564f0
aj-t-swoboda <aj.t.swoboda@gmail.com>
2016-06-04 08:57:55 -0700

    added stadium model, with name and capacity fields, and FK on game model

--- app/games/models.py
+++ app/games/models.py
@@ -9,5 +9,20 @@ class Game(models.Model):
 	"""
 	date = models.DateField("date played")
 
+	stadium = models.ForeignKey("Stadium")
+
 	def __str__(self):
 		return "%s" % self.date
+
+
+# Create Stadium model
+class Stadium(models.Model):
+	"""
+	Make a model for all stadiums used
+	"""
+
+	name = models.CharField("Stadium Name", max_length = 200)
+	capacity = models.IntegerField("Capacity", default = 0)
+
+	def __str__(self):
+		return self.name
