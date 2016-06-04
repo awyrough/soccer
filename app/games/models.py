01a888d74fa4aff5a804a41c0b074e8e1ab12448
awyrough <alexander.hill.wyrough@gmail.com>
2016-06-04 11:12:22 -0400

    Adds game model to app

--- app/games/models.py
+++ app/games/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+	"""
+	Model for MLS Games and meta information.
+	"""
+	date = models.DateField("date played")
+
+	def __str__(self):
+		return "%s" % self.date
