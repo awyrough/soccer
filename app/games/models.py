942085c0c3822a3498dbf7f786cd4f7054afdae6
aj-t-swoboda <aj.t.swoboda@gmail.com>
2016-06-05 12:06:14 -0700

    added a Team Model to DB

--- app/games/models.py
+++ app/games/models.py
@@ -8,12 +8,28 @@ class Game(models.Model):
 	Model for MLS Games and meta information.
 	"""
 	date = models.DateField("date played")
-
 	stadium = models.ForeignKey("Stadium")
 
 	def __str__(self):
 		return "%s" % self.date
 
+# Create Team model
+class Team(models.Model):
+	"""
+	Model for each soccer team
+	"""
+	#teamID automatically made
+	name = models.CharField("Name", max_length = 50)
+	conference = models.CharField("Conference", max_length = 20, null=True)
+	city = models.CharField("City", max_length = 50, null=True, blank=True)
+	state = models.CharField("State", max_length = 20, null=True, blank=True)
+	yearJoined = models.IntegerField("yearJoined", default = -9999, null=True)
+
+	"""
+	Notes:
+		blank=True   <-  if T, then not required in form
+		null=True 	 <-  if T, then can be stored as Null
+	"""
 
 # Create Stadium model
 class Stadium(models.Model):
