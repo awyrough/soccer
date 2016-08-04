fixed the aggregate, metric, and lift time type functions. now they work as inputs in the command line. also validated that the math for Total, pass_accuracy, average works

app/events/analysis/aggregators.py
@@ -1,33 +1,39 @@
 from events.utils.time_and_statistic import *
 import numpy as np
+import numbers
 
 # Define the 'metric' here
 PASSES = lambda x: x.passes
 GOALS = lambda x: x.goals
-PASS_ACCURACY = lambda x: x.passes_unsucc / x.passes
+PASS_ACCURACY = lambda x: float(x.passes_succ) / float(x.passes) if x.passes > 0 else None
 
 # Define the 'aggregation' here (average, median, total)
-COUNT = lambda x: sum(x)
+SUM = lambda x: sum(x)
 AVERAGE = lambda x: np.mean(x)
 
-# Set some defaults for sanity and testing
-DEFAULT_METRIC = PASSES
-DEFAULT_AGGREGATE = COUNT
-
 def pass_accuracy_improved(event):
-	return event.passes_unsucc / event.passes if event.passes > 0 else 0
+	return float(event.passes_succ) / float(event.passes) if event.passes > 0 else None
 
-def metric_command(metric_string):
-	m_c = {}
-	m_c["passes"] = PASSES
-	m_c["goals"] = GOALS
-	m_c["pass_accuracy"] = PASS_ACCURACY
+def average_with_nones(values):
+	non_null = [val for val in values if isinstance(val, numbers.Number)]
+	if non_null:
+		return sum(non_null) / len(non_null)
+	return None
 
-	return m_c[metric_string]
+MAP_METRIC_FCN = {
+	"passes": PASSES
+	,"goals": GOALS
+	,"pass_accuracy": PASS_ACCURACY
+}
+
+MAP_AGGREGATE_FCN = {
+	"sum": SUM
+	,"average": average_with_nones
+}
 
 def do_collect_and_aggregate(
 		team, game, windows, meta_info, agg_tally_moments,
-		metric=DEFAULT_METRIC, aggregate=DEFAULT_AGGREGATE):
+		metric_fcn, aggregate_fcn):
 	"""
 	Iterate over windows and collect stats, bucket by windows,
 	and run an aggregation.
@@ -43,7 +49,7 @@ def do_collect_and_aggregate(
 		events = get_game_time_events_for_team(
 			game, team, start_minute=start, end_minute=end)
 		time_hash = "[%s, %s]" % (start, end)
-		agg_metric = aggregate([metric(event) for event in events])
+		agg_metric = aggregate_fcn([metric_fcn(event) for event in events])
 		collection[window_count] = (agg_metric, time_hash, time_window_length((start,end)) \
 				, meta_info[window_count], agg_tally_moments[window_count])
 		window_count += 1

app/events/analysis/statistics.py
@@ -2,29 +2,37 @@ import numpy as np
 from scipy.stats import ttest_1samp, wilcoxon, ttest_ind, mannwhitneyu
 
 
-def lift(pre_value, post_value):
+def LIFT_TOTAL(previous, current):
 	"""
-	Define, mathematically, how we want to calculate lift between T-1 and T
-	"""
-	return (post_value - pre_value) / pre_value
+	At a total level calculate lift between T-1 and T
 
-def get_time_type_code(time_type):
-	known_types = ["Total", "Per Min"]
-	if time_type not in known_types:
-		raise Exception("Unknown time type")
-	if time_type == "Total":
-		return 0
-	elif time_type == "Per Min":
-		return 1
-	else:
-		return None
+	Inputs are lists of the form: (350, '[0, -2]', 95.5, 'Meta Info', 0)
 
-def calculate_lift(games, agg_collection, time_type, min_time_window):
 	"""
-	Time Types:
-		0 = Total
-		1 = Per Min 
-		(?) 2 = Per Game
+	post = float(current[0])
+	pre = float(previous[0])
+
+	return (post - pre) / pre
+
+def LIFT_PER_MIN(previous, current):
+	"""
+	At a per-min level calculate lift between T-1 and T
+
+	Inputs are lists of the form: (350, '[0, -2]', 95.5, 'Meta Info', 0)
+
+	"""
+	post = float(current[0])/float(current[2])
+	pre = float(previous[0])/float(previous[2])
+
+	return (post - pre) / pre
+
+MAP_LIFT_TYPE_FCN = {
+	"Total": LIFT_TOTAL
+	,"Per Min": LIFT_PER_MIN
+}
+
+def calculate_lift(games, agg_collection, lift_fcn, min_time_window):
+	"""
 	"""
 	calculated_lifts = []
 
@@ -39,18 +47,10 @@ def calculate_lift(games, agg_collection, time_type, min_time_window):
 			# if the window is long enough
 			if current[2] > min_time_window:
 				# if this game has a legitimate previous time window to compare to
-				# and if the pre won't be 0 (as you can't calculate)
-				if previous and previous[0] != 0:
-					if time_type == 0:
-						pre = float(previous[0])
-						post = float(current[0])
-					elif time_type == 1:
-						pre = float(previous[0]) / previous[2]
-						post = float(current[0]) / current[2]						
-					else: #other cases we haven't planned for yet
-						pre = None
-						post = None
-					lift_value = lift(pre, post)
+				# and if the pre value won't be 0 (as you can't calculate)
+				# and if the pre value isn't None (in the case of all ratios being None)
+				if previous and previous[0] != 0 and previous[0] and current[0]:
+					lift_value = lift_fcn(previous, current)
 
 					calculated_lifts.append((lift_value, str(game.date)))
 					agg_collection[game][index] = agg_collection[game][index] + (lift_value,)